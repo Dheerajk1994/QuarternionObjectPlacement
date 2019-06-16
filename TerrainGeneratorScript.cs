3f2567041c38c6218f86157a474cdc4f03c1baf5
D <dheerajk.har@gmail.com>
2019-06-15 19:38:02 -0700

    code improvement
    Now the forward direction does not have to be messed around with.

--- TerrainGeneratorScript.cs
+++ TerrainGeneratorScript.cs
@@ -13,12 +13,12 @@ public class TerrainGeneratorScript : MonoBehaviour
 
     private void Start()
     {
-        worldCenter = world.transform.position;
+         worldCenter = world.transform.position;
         GameObject tempObj;
         for(int i = 0; i < numberOfTrees; ++i)
         {
             Vector3 pos = Random.onUnitSphere.normalized * distanceFromCenter - worldCenter;
-            Quaternion rotation = Quaternion.LookRotation(pos - worldCenter);
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.up, pos - worldCenter);
             tempObj = Instantiate(treePrefab, pos, rotation);
         }
     }
